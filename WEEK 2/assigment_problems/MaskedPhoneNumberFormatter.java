748330e2b0c02191cc4f7865132a4d24ad06e4da
Shadman <shadmanmd48@gmail.com>
2026-09-05 16:02:21 +0530

    feat(session_2): add week 2 class and assignment problems

---
+++ WEEK 2/assigment_problems/MaskedPhoneNumberFormatter.java
@@ -0,0 +1,26 @@
+public class MaskedPhoneNumberFormatter {
+
+    public static String maskPhoneNumber(String phone) {
+        if (phone == null || phone.length() != 10) {
+            return "Invalid phone number";
+        }
+
+        for (int i = 0; i < phone.length(); i++) {
+            if (!Character.isDigit(phone.charAt(i))) {
+                return "Invalid phone number";
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("XXXXXX");
+        sb.append(phone.substring(6));
+        sb.insert(6, "-");
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maskPhoneNumber("9876543210"));
+        System.out.println(maskPhoneNumber("98765"));
+    }
+}
